2382fd40e6bbf7784c2081fcd0721ec5facf04e3
Akbar Khan <akhan@enine.dev>
2025-09-25 10:48:00 +0530

    feat(gizmo): add API call retry logic, change timestamp to date.

--- apps/gizmo/utils.py
+++ apps/gizmo/utils.py
@@ -1,4 +1,5 @@
 import os
+import logging
 from datetime import date, datetime
 import requests
 from dateutil.relativedelta import relativedelta
@@ -13,13 +14,15 @@ from django.core.cache import cache
 
 load_dotenv()
 
+logger = logging.getLogger(__name__)
+
 
 class ProcessData:
     def __init__(self, data):
         self.df = pd.DataFrame(data)
+        self.df["refdate"] = pd.to_datetime(self.df["refdate"])
 
     def aggregate(self, df):
-        df["refdate"] = pd.to_datetime(df["refdate"])
         return (
             df.groupby(pd.Grouper(key="refdate", freq="W"))
             .agg({"price": "last", "volume": "sum"})
@@ -29,8 +32,10 @@ class ProcessData:
     def clean(self):
         return self.df[["refdate", "price", "volume"]]
 
-    def to_json(self):
-        return self.df.to_json(orient="records", date_format="iso")
+    def to_json(self, df):
+        df["refdate"] = df["refdate"].dt.strftime("%Y-%m-%d")
+        df = df.fillna(0)
+        return df.to_json(orient="records", date_format="iso")
 
 
 class ClubData:
@@ -40,6 +45,7 @@ class ClubData:
         self.request_timeout = int(os.getenv("REQUEST_TIMEOUT"))
         self.api_token = os.getenv("API_TOKEN")
         self.update = update
+        self.max_retries = int(os.getenv("MAX_RETRIES"))
 
     def get_params(self):
         return [
@@ -79,21 +85,38 @@ class ClubData:
 
         headers = {"Authorization": f"Token {self.api_token}"}
         for idx, param in enumerate(self.get_params(), start=1):
-            response = requests.get(
-                self.url, params=param, headers=headers, timeout=self.request_timeout
-            )
-            if response.status_code == 200:
-                data = response.json()
+            i = 0
+            response = None
 
-                process_data = ProcessData(data=data)
-                processed_data = process_data.clean()
+            while i < self.max_retries:
+                try:
+                    response = requests.get(
+                        self.url,
+                        params=param,
+                        headers=headers,
+                        timeout=self.request_timeout,
+                    )
 
-                if len(data) > 100:
-                    processed_data = process_data.aggregate(processed_data)
+                    if response.status_code == 200:
+                        break
+                except requests.exceptions.RequestException as _:
+                    pass
 
-                clubbed_data_dict[f"data_{idx}"] = processed_data.fillna(0).to_dict(
-                    orient="records"
-                )
+                i += 1
+
+            if not response or response.status_code != 200:
+                logger.info("Unable to fetch data for %s", param)
+                continue
+
+            data = response.json()
+
+            process_data = ProcessData(data=data)
+            processed_data = process_data.clean()
+
+            if len(data) > 100:
+                processed_data = process_data.aggregate(processed_data)
+
+            clubbed_data_dict[f"data_{idx}"] = process_data.to_json(processed_data)
 
         cache_timeout_seconds = self.__class__.get_cache_timeout_seconds()
         cache.set(key, clubbed_data_dict, timeout=cache_timeout_seconds)
